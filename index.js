e7bd2bc7bca115eaeb32ee5e0db93f8aad9e1a71
Nitesh Gupta <Niteshgupta202122@gmail.com>
2023-01-27 19:04:53 +0530

    second commit

--- index.js
+++ index.js
@@ -146,6 +146,16 @@ app.post('/api/verify/otpvalid',(req,res)=>{
   });
 });
 
+app.use(express.static(path.join(__dirname,"./client/build")));
+
+app.get("*",function (_,res){
+    res.sendFile(
+      path.join(__dirname,"./client/build/index.html"),
+      function(err){
+        res.status(500).send(err);
+      }
+    );
+});
 
 app.listen(port,()=>{
     console.log(`Server is working at ${port}`);
